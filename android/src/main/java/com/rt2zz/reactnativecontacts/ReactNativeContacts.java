6b03cb0aa5d06b40dd7344108e4b7a0884c12c91
Rodrigo Bechara <rodribech20@gmail.com>
2017-10-25 12:27:00 -0300

    package support RN44

--- android/src/main/java/com/rt2zz/reactnativecontacts/ReactNativeContacts.java
+++ android/src/main/java/com/rt2zz/reactnativecontacts/ReactNativeContacts.java
@@ -1,12 +1,14 @@
 package com.rt2zz.reactnativecontacts;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ReactNativeContacts implements ReactPackage {
@@ -23,5 +25,10 @@ public class ReactNativeContacts implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
     }
 }
